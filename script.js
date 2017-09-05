f0b4ccbeef9e71a86b7831be5b61ff1fc0c2cfb8
Toke Stahl Jacobsen <Stahl@Tokes-MacBook-Pro.local>
2017-09-05 09:56:21 -0500

    Test

---
+++ script.js
@@ -0,0 +1,68 @@
+//Lecture: variables
+/*
+var name = "John";
+console.log(name);
+
+var lastName = "Smith";
+console.log(lastName);
+
+var age = 26;
+console.log(age);
+
+var fullAge = true;
+console.log(fullAge);
+*/
+
+//Lecture: variables 2
+/*
+var name = "John";
+var age = 26;
+
+//console.log(name + age);
+//console.log(age + age)
+
+var job, isMarried;
+
+//console.log(job)
+
+job = "teacher";
+isMarried = false; 
+
+console.log(name + " is a " + age + " year old " + job + ". Is he married? " + isMarried + " . ");
+
+age = "thirty six";
+job = "driver";
+
+console.log(name + " is a " + age + " year old " + job + ". Is he married? " + isMarried + " . ");
+
+//var lastName = prompt("What is the last name?");
+//console.log(lastName);
+
+alert(name + " is a " + age + " year old " + job + ". Is he married? " + isMarried + " . ")
+*/
+
+//Lecture: Operators
+var birthYear = 2017 - 26;
+var currentYear = 2017;
+
+birthYear = currentYear - 26 * 2;
+//2017 - 52 = 1965
+
+console.log(birthYear);
+
+var ageJohn = 30;
+var ageMarc = 30;
+
+ageJohn = ageMarc = (3 + 5) * 4 - 6;
+//ageJohn = ageMarc = 26
+console.log(ageJohn);
+console.log(ageMarc);
+
+ageJohn++;
+ageMarc *= 2;
+
+console.log(ageMarc);
+console.log(ageJohn);
+
+
+
